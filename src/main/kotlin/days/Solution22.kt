0434a31c6f65b84c9af3f85165a733ffedbc9c1a
Verulean <65474222+Verulean@users.noreply.github.com>
2024-12-22 01:07:57 -0500

    Day 22

---
+++ src/main/kotlin/days/Solution22.kt
@@ -0,0 +1,41 @@
+package days
+
+import adventOfCode.InputHandler
+import adventOfCode.Solution
+import adventOfCode.util.longs
+
+object Solution22 : Solution<List<Long>>(AOC_YEAR, 22) {
+    override fun getInput(handler: InputHandler) = handler.getInput().longs()
+
+    private const val PRUNE_MODULO = 16777216L
+    private const val TRIGGER_LENGTH = 4
+
+    private fun Long.evolve(): Long {
+        var n = this
+        n = n xor (n shl 6) % PRUNE_MODULO
+        n = n xor (n shr 5) % PRUNE_MODULO
+        n = n xor (n shl 11) % PRUNE_MODULO
+        return n
+    }
+    private val Long.price get() = if (this >= 0) this % 10 else -(-this % 10)
+
+    override fun solve(input: List<Long>): Pair<Any?, Any?> {
+        var ans1 = 0L
+        val profits: MutableMap<List<Long>, Long> = mutableMapOf()
+        input.forEach {
+            var n = it
+            val prices: MutableList<Long> = mutableListOf()
+            repeat(2000) {
+                n = n.evolve()
+                prices.add(n.price)
+            }
+            ans1 += n
+            val deltas = prices.zipWithNext { a, b -> b - a }
+            val seenTriggers: MutableSet<List<Long>> = mutableSetOf()
+            deltas.windowed(TRIGGER_LENGTH).zip(prices.drop(TRIGGER_LENGTH)).forEach { (trigger, price) ->
+                if (seenTriggers.add(trigger)) profits.merge(trigger, price, Long::plus)
+            }
+        }
+        return ans1 to profits.values.max()
+    }
+}
